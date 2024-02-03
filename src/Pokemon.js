b2dac6db9fcce2697d032e049e6e85368e745f07
Kenjebai <142065688+Kenjebai@users.noreply.github.com>
2024-02-03 18:23:09 +0600

    Add files via upload

---
+++ src/Pokemon.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export default function Pokemon(){
+    const[list, setList] = useState([])
+    const addList = async() => {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?_limit=6")
+        const data = await response.json()
+        setList(data.results)
+        console.log(data.results);
+    }
+    return(
+        <div>
+            <h2>Pokemon</h2>
+            <button onClick={addList}>add</button>
+                <div className="post">
+                    {list.map((el) => (
+                        <div className="list">
+                            <img src={el.url} alt="photo"/>
+                            <span>{el.name}</span>
+                        </div>
+                    ))}
+                </div>
+        </div>
+    )
+}
+
